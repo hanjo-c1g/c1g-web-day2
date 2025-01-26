385eaed190830a30b582f6e0297d8b8d1942e91d
Hanjo Meinhardt <hanjo@bunix.de>
2025-01-26 20:42:05 +0100

    add solution for task 1

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  // task 1 change background color
   const changeColorButton = document.getElementById('changeColorButton');
+  const resetColorButton = document.getElementById('resetColorButton');
+  const colors = ["lightblue", "lightgreen", "lightcoral", "lightgoldenrodyellow"];
 
   changeColorButton.addEventListener('click', () => {
-    alert('Hier könnte die Hintergrundfarbe verändert werden!');
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+  });
+
+  resetColorButton.addEventListener('click', () => {
+    document.body.style.removeProperty('background-color');
   });
 
 });
